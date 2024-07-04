e314ef04230a7f4d8f59196970b1f98a43256ca9
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2024-07-05 00:40:16 +0300

    feat: setup audio input and recording

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyaudio
 import wave
 
 format = pyaudio.paInt16
-channels = 2
+channels = 1
 rate = 44100  # number of samples
 chunk = 1024 #  number of frames in the buffer
 record_seconds = 5
@@ -21,5 +21,17 @@ frames = []
 
 for i in range(0, int(rate/chunk*record_seconds)):
     data = stream.read(chunk)  # read no. of frames from audio stream
-    frames.append(data) # appends data to the frame list
+    frames.append(data)  # appends data to the frame list
 print("Recording Finished...")
+
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+waveFile = wave.open(wave_output_file, 'wb')
+waveFile.setnchannels(channels)
+waveFile.setsampwidth(audio.get_sample_size(format))
+waveFile.setframerate(rate)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
