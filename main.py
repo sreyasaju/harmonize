9e212f539814c6de4eafad11411fe6c1b1fb03db
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2024-07-05 02:09:53 +0300

    feat: setup pitch detection

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pyaudio
 import wave
+import aubio
 
 format = pyaudio.paInt16
 channels = 1
@@ -15,23 +17,44 @@ stream = audio.open(format=format,
                     rate=rate,
                     input=True,
                     frames_per_buffer=chunk)
+
+
+tolerance = 0.9
+fft_size = 4096
+hop_size = chunk
+pitch_o = aubio.pitch("default", fft_size, hop_size, rate)
+pitch_o.set_unit("midi")
+pitch_o.set_tolerance(tolerance)
 print("Recording..")
 
 frames = []
 
-for i in range(0, int(rate/chunk*record_seconds)):
-    data = stream.read(chunk)  # read no. of frames from audio stream
-    frames.append(data)  # appends data to the frame list
-print("Recording Finished...")
 
+try:
+    for _ in range(0, int(rate / chunk * record_seconds)):
+        audio_buffer = stream.read(chunk)  # read no. of frames from audio stream
+        frames.append(audio_buffer)
 
-stream.stop_stream()
-stream.close()
-audio.terminate()
+        signal = np.frombuffer(audio_buffer, dtype=np.float32)
 
-waveFile = wave.open(wave_output_file, 'wb')
-waveFile.setnchannels(channels)
-waveFile.setsampwidth(audio.get_sample_size(format))
-waveFile.setframerate(rate)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
+        pitch = pitch_o(signal)[0]
+        confidence = pitch_o.get_confidence()
+
+        print("Pitch: {}, Confidence: {:.2f}".format(pitch, confidence))
+    print("Recording Finished...")
+
+except KeyboardInterrupt:
+    print("Recording Interrupted")
+finally:
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    waveFile = wave.open(wave_output_file, 'wb')
+    waveFile.setnchannels(channels)
+    waveFile.setsampwidth(audio.get_sample_size(format))
+    waveFile.setframerate(rate)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+
+    print(f"Saved recording to {wave_output_file}")
