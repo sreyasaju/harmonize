923f44218fae5d4027001e1ca71960336d8b9b9e
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-11-01 11:36:41 +0300

    fix: update MIDI logic

--- midi.py
+++ midi.py
@@ -10,7 +10,7 @@ def midi_to_alphabet(midi_note):
     if 0 <= midi_note <= 127:
         note_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
         
-        # MIDI note 0 is C in octave -1 
+        # mIDI note 0 is C in octave -1 
         octave = midi_note // 12 - 1
         note = note_names[midi_note % 12]
         return f"{note}{octave}"
@@ -19,7 +19,7 @@ def midi_to_alphabet(midi_note):
 def convert_to_midi(wave_output_file, midi_output, silence_threshold=-40.0):
     signal, sr = librosa.load(wave_output_file, sr=None)
 
-    # calculate the RMS energy of the signal, to diff singing and noise...
+    # Calculate the RMS energy of the signal
     rms = librosa.feature.rms(y=signal, frame_length=2048, hop_length=512)
     rms_db = librosa.amplitude_to_db(rms, ref=np.max)
 
@@ -33,57 +33,40 @@ def convert_to_midi(wave_output_file, midi_output, silence_threshold=-40.0):
 
     hop_length = 512
     ticks_per_beat = midi_file.ticks_per_beat
-    onset_env = librosa.onset.onset_strength(y=signal, sr=sr) # to future me, https://librosa.org/doc/0.11.0/generated/librosa.onset.onset_strength.html#librosa.onset.onset_strength
-
-    # using the librosa beat detection function, rather than hardcoding it...
-    tempo_array = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
-    estimated_tempo = tempo_array[0] if len(tempo_array) > 0 else 120.0
-    print(estimated_tempo)
-    if estimated_tempo == 0 or np.isnan(estimated_tempo):
-        estimated_tempo = 120.0  # fallback for weird cases like whispering
-
+    tempo = 500000  # microseconds per beat
+    ticks_per_second = (ticks_per_beat * 1000000) // tempo
 
     last_pitch = None
     last_time = 0
 
     for i, (pitch, voiced_flag) in enumerate(zip(pitches, voiced_flags)):
         rms_value = rms_db[0][i]
-        current_time = int((i * hop_length) / sr * ticks_per_beat * 60 / estimated_tempo)
-
-        if voiced_flag and pitch is not None and not np.isnan(pitch) and rms_value > silence_threshold:
+        if voiced_flag and rms_value > silence_threshold:
             midi_note = freq_to_midi(pitch)
+            current_time = int((i * hop_length) / sr * ticks_per_second)
 
-            if last_pitch is None:
-                track.append(Message('note_on', note=midi_note, velocity=64, time=0))
-                last_pitch = midi_note
-                last_time = current_time
-
-            elif last_pitch != midi_note:
+            if last_pitch is not None and last_pitch != midi_note:
+                # note off for the previous note
                 duration = current_time - last_time
-                if duration < 0:
-                    duration = 0
                 track.append(Message('note_off', note=last_pitch, velocity=64, time=duration))
+                last_time = current_time
+
+            # note on for the current note
+            if last_pitch != midi_note:
                 track.append(Message('note_on', note=midi_note, velocity=64, time=0))
                 last_pitch = midi_note
                 last_time = current_time
 
-            alphabet = midi_to_alphabet(midi_note)
-            if alphabet:
-                print(f"Pitch: {pitch:.2f}, MIDI Note: {alphabet}")
-            else:
-                print(f"No alphabet mapping found for MIDI Note {midi_note}")
-
-        elif last_pitch is not None:
-            duration = current_time - last_time
-            if duration < 0:
-                duration = 0
-            track.append(Message('note_off', note=last_pitch, velocity=64, time=duration))
-            last_pitch = None
+                alphabet = midi_to_alphabet(midi_note)
+                if alphabet:
+                    print(f"Pitch: {pitch:.2f}, MIDI Note: {alphabet}")
+                else:
+                    print(f"No alphabet mapping found for MIDI Note {midi_note}")
 
      # note off for the last note
     if last_pitch is not None:
         track.append(Message('note_off', note=last_pitch, velocity=64, time=0))
 
-    # save the masterpiece ;)
+    # save the MIDI file
     midi_file.save(midi_output)
     print(f"Saved MIDI to {midi_output}")
---
+++ res_rc.py
@@ -0,0 +1,370 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.7.2
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x02\x22\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x224\
+8px\x22 viewBox=\x220 \
+-960 960 960\x22 wi\
+dth=\x2248px\x22 fill=\
+\x22#e8eaed\x22><path \
+d=\x22M480.18-387.3\
+q-55.99 0-93.98-\
+39.75-38-39.74-3\
+8-95.95v-244.9q0\
+-54.32 38.22-92.\
+73 38.21-38.4 93\
+.47-38.4 55.25 0\
+ 93.77 38.4 38.5\
+1 38.41 38.51 92\
+.73V-523q0 56.21\
+-37.99 95.95-38 \
+39.75-94 39.75ZM\
+435.45-80.38v-13\
+3.01q-118.62-13.\
+33-195.94-101.57\
+Q162.2-403.2 162\
+.2-523h89.47q0 9\
+4.36 66.84 159.5\
+8 66.85 65.22 16\
+1.69 65.22 94.85\
+ 0 161.49-65.3 6\
+6.64-65.3 66.64-\
+159.5h89.47q0 12\
+0.04-77.7 208.16\
+-77.69 88.12-195\
+.75 101.41v133.0\
+5h-88.9Z\x22/></svg\
+>\
+\x00\x00\x01N\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x224\
+8px\x22 viewBox=\x220 \
+-960 960 960\x22 wi\
+dth=\x2248px\x22 fill=\
+\x22#e8eaed\x22><path \
+d=\x22M295-244v-478\
+q0-22 14.07-35t3\
+2.5-13q6.2 0 12.\
+81 1.5 6.62 1.5 \
+13.5 5.43L744-52\
+2q10.5 7 16.25 1\
+7t5.75 22q0 12-5\
+.75 22T744-444L3\
+67.88-202.93Q361\
+-199 354.36-198q\
+-6.64 1-12.86 1-\
+18.5 0-32.5-12.2\
+7-14-12.28-14-34\
+.73Z\x22/></svg>\
+\x00\x00\x03\xc0\
+<\
+svg width=\x2298\x22 h\
+eight=\x2296\x22 xmlns\
+=\x22http://www.w3.\
+org/2000/svg\x22><p\
+ath fill-rule=\x22e\
+venodd\x22 clip-rul\
+e=\x22evenodd\x22 d=\x22M\
+48.854 0C21.839 \
+0 0 22 0 49.217c\
+0 21.756 13.993 \
+40.172 33.405 46\
+.69 2.427.49 3.3\
+16-1.059 3.316-2\
+.362 0-1.141-.08\
+-5.052-.08-9.127\
+-13.59 2.934-16.\
+42-5.867-16.42-5\
+.867-2.184-5.704\
+-5.42-7.17-5.42-\
+7.17-4.448-3.015\
+.324-3.015.324-3\
+.015 4.934.326 7\
+.523 5.052 7.523\
+ 5.052 4.367 7.4\
+96 11.404 5.378 \
+14.235 4.074.404\
+-3.178 1.699-5.3\
+78 3.074-6.6-10.\
+839-1.141-22.243\
+-5.378-22.243-24\
+.283 0-5.378 1.9\
+4-9.778 5.014-13\
+.2-.485-1.222-2.\
+184-6.275.486-13\
+.038 0 0 4.125-1\
+.304 13.426 5.05\
+2a46.97 46.97 0 \
+0 1 12.214-1.63c\
+4.125 0 8.33.571\
+ 12.213 1.63 9.3\
+02-6.356 13.427-\
+5.052 13.427-5.0\
+52 2.67 6.763.97\
+ 11.816.485 13.0\
+38 3.155 3.422 5\
+.015 7.822 5.015\
+ 13.2 0 18.905-1\
+1.404 23.06-22.3\
+24 24.283 1.78 1\
+.548 3.316 4.481\
+ 3.316 9.126 0 6\
+.6-.08 11.897-.0\
+8 13.526 0 1.304\
+.89 2.853 3.316 \
+2.364 19.412-6.5\
+2 33.405-24.935 \
+33.405-46.691C97\
+.707 22 75.788 0\
+ 48.854 0z\x22 fill\
+=\x22#fff\x22/></svg>\
+\x00\x00\x01\xf7\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x224\
+8px\x22 viewBox=\x220 \
+-960 960 960\x22 wi\
+dth=\x2248px\x22 fill=\
+\x22#e8eaed\x22><path \
+d=\x22M637.65-154q-\
+39.05 0-66.85-27\
+.91Q543-209.81 5\
+43-249v-462q0-39\
+.19 27.82-67.09Q\
+598.63-806 637.6\
+9-806h55q39.06 0\
+ 66.69 27.91Q787\
+-750.19 787-711v\
+462q0 39.19-27.6\
+4 67.09Q731.71-1\
+54 692.65-154h-5\
+5Zm-370 0q-39.05\
+ 0-66.85-27.91Q1\
+73-209.81 173-24\
+9v-462q0-39.19 2\
+7.82-67.09Q228.6\
+3-806 267.69-806\
+h55q39.06 0 66.6\
+9 27.91Q417-750.\
+19 417-711v462q0\
+ 39.19-27.64 67.\
+09Q361.71-154 32\
+2.65-154h-55Z\x22/>\
+</svg>\
+\x00\x00\x05 \
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22UTF\
+-8\x22 standalone=\x22\
+no\x22?>\x0a<svg\x0a   he\
+ight=\x2248\x22\x0a   vie\
+wBox=\x220 -960 960\
+ 960\x22\x0a   width=\x22\
+48\x22\x0a   fill=\x22#e8\
+eaed\x22\x0a   version\
+=\x221.1\x22\x0a   id=\x22sv\
+g1\x22\x0a   sodipodi:\
+docname=\x22mic.svg\
+\x22\x0a   inkscape:ex\
+port-filename=\x22i\
+con.svg\x22\x0a   inks\
+cape:export-xdpi\
+=\x2248.713909\x22\x0a   \
+inkscape:export-\
+ydpi=\x2248.713909\x22\
+\x0a   xmlns:inksca\
+pe=\x22http://www.i\
+nkscape.org/name\
+spaces/inkscape\x22\
+\x0a   xmlns:sodipo\
+di=\x22http://sodip\
+odi.sourceforge.\
+net/DTD/sodipodi\
+-0.dtd\x22\x0a   xmlns\
+=\x22http://www.w3.\
+org/2000/svg\x22\x0a  \
+ xmlns:svg=\x22http\
+://www.w3.org/20\
+00/svg\x22>\x0a  <defs\
+\x0a     id=\x22defs1\x22\
+ />\x0a  <sodipodi:\
+namedview\x0a     i\
+d=\x22namedview1\x22\x0a \
+    pagecolor=\x22#\
+505050\x22\x0a     bor\
+dercolor=\x22#eeeee\
+e\x22\x0a     borderop\
+acity=\x221\x22\x0a     i\
+nkscape:showpage\
+shadow=\x220\x22\x0a     \
+inkscape:pageopa\
+city=\x220\x22\x0a     in\
+kscape:pagecheck\
+erboard=\x220\x22\x0a    \
+ inkscape:deskco\
+lor=\x22#505050\x22>\x0a \
+   <inkscape:pag\
+e\x0a       x=\x220\x22\x0a \
+      y=\x220\x22\x0a    \
+   width=\x22960\x22\x0a \
+      height=\x2296\
+0\x22\x0a       id=\x22pa\
+ge2\x22\x0a       marg\
+in=\x220\x22\x0a       bl\
+eed=\x220\x22 />\x0a  </s\
+odipodi:namedvie\
+w>\x0a  <circle\x0a   \
+  style=\x22fill:#4\
+5259b;fill-opaci\
+ty:1;fill-rule:e\
+venodd;stroke-wi\
+dth:34.5504;stro\
+ke-linecap:round\
+;stroke-linejoin\
+:round\x22\x0a     id=\
+\x22path2\x22\x0a     cx=\
+\x22-480\x22\x0a     cy=\x22\
+-480\x22\x0a     inksc\
+ape:export-filen\
+ame=\x22icon.svg\x22\x0a \
+    inkscape:exp\
+ort-xdpi=\x2248.713\
+909\x22\x0a     inksca\
+pe:export-ydpi=\x22\
+48.713909\x22\x0a     \
+transform=\x22scale\
+(-1,1)\x22\x0a     r=\x22\
+480\x22 />\x0a</svg>\x0a\
+\x00\x00\x01\x9d\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x224\
+8px\x22 viewBox=\x220 \
+-960 960 960\x22 wi\
+dth=\x2248px\x22 fill=\
+\x22#e8eaed\x22><path \
+d=\x22M386-96q-69.5\
+2 0-116.76-47.24\
+Q222-190.48 222-\
+259.5q0-69.02 47\
+.24-116.76Q316.4\
+8-424 386-424q21\
+.85 0 40.43 6Q44\
+5-412 459-402v-4\
+17q0-18.47 12.5-\
+32.24Q484-865 50\
+4-865h190q18.6 0\
+ 32.3 13.76Q740-\
+837.47 740-819v7\
+4q0 19.6-13.7 32\
+.8Q712.6-699 694\
+-699H550v439q0 6\
+9.52-48.24 116.7\
+6Q453.51-96 386-\
+96Z\x22/></svg>\
+\x00\x00\x01,\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x224\
+8px\x22 viewBox=\x220 \
+-960 960 960\x22 wi\
+dth=\x2248px\x22 fill=\
+\x22#e8eaed\x22><path \
+d=\x22M215-309v-342\
+q0-40.21 27.1-67\
+.61Q269.2-746 30\
+9-746h342q40.21 \
+0 67.61 27.39Q74\
+6-691.21 746-651\
+v342q0 39.8-27.3\
+9 66.9Q691.21-21\
+5 651-215H309q-3\
+9.8 0-66.9-27.1Q\
+215-269.2 215-30\
+9Z\x22/></svg>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x02\
+\x00\x00\x07\xb9\
+\x00u\
+\x00i\
+\x00\x07\
+\x03\xf6Z'\
+\x00m\
+\x00i\x00c\x00.\x00s\x00v\x00g\
+\x00\x08\
+\x02\x8cT'\
+\x00p\
+\x00l\x00a\x00y\x00.\x00s\x00v\x00g\
+\x00\x0a\
+\x0f\xb8\x9b\x87\
+\x00g\
+\x00i\x00t\x00h\x00u\x00b\x00.\x00s\x00v\x00g\
+\x00\x09\
+\x0c\x98\xb7\xc7\
+\x00p\
+\x00a\x00u\x00s\x00e\x00.\x00s\x00v\x00g\
+\x00\x08\
+\x0aaW'\
+\x00i\
+\x00c\x00o\x00n\x00.\x00s\x00v\x00g\
+\x00\x0b\
+\x0cC\x91G\
+\x00c\
+\x00o\x00n\x00v\x00e\x00r\x00t\x00.\x00s\x00v\x00g\
+\x00\x08\
+\x0bcU\x87\
+\x00s\
+\x00t\x00o\x00p\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x07\x00\x00\x00\x04\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00.\x00\x00\x00\x00\x00\x01\x00\x00\x02&\
+\x00\x00\x01\x91\xa9N\x95\x17\
+\x00\x00\x00\x1a\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x91\xa9N\x95\x17\
+\x00\x00\x00v\x00\x00\x00\x00\x00\x01\x00\x00\x097\
+\x00\x00\x01\x91\xa9\x8b\x00\xc7\
+\x00\x00\x00\xa8\x00\x00\x00\x00\x00\x01\x00\x00\x0f\xfc\
+\x00\x00\x01\x91\xa9N\x95\x18\
+\x00\x00\x00\x8c\x00\x00\x00\x00\x00\x01\x00\x00\x0e[\
+\x00\x00\x01\x91\xa9N\x95\x17\
+\x00\x00\x00^\x00\x00\x00\x00\x00\x01\x00\x00\x07<\
+\x00\x00\x01\x91\xa9N\x95\x17\
+\x00\x00\x00D\x00\x00\x00\x00\x00\x01\x00\x00\x03x\
+\x00\x00\x01\x91\xa9N\x95\x17\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
