964feb21ee7a29fe7d853bda73012e6e4030e6bc
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2026-06-05 19:36:16 +0300

    style: fix the indentation which was off throughout

--- midi_playback.py
+++ midi_playback.py
@@ -14,93 +14,85 @@ from matplotlib.patches import Rectangle
 
 
 class MidiPlayback(QFrame):
-        def __init__(self, midiframe, parent=None):
-            super().__init__(parent)
+    def __init__(self, midiframe, parent=None):
+        super().__init__(parent)
 
-            self.midiframe = midiframe
+        self.midiframe = midiframe
 
+        self.fig = Figure(figsize=(10, 2), dpi=100)  # 10in2in would do
+        self.ax = self.fig.add_subplot(111)
+        self.fig.subplots_adjust(
+            left=0.03,
+            right=0.97,
+            top=0.92,
+            bottom=0.08,
+        )
 
-            self.fig = Figure(figsize=(10, 2), dpi=100) #10in2in would do
-            self.ax = self.fig.add_subplot(111)
-            self.fig.subplots_adjust(
-                left=0.03,
-                right=0.97,
-                top=0.92,
-                bottom=0.08
-            )
+        self.ax.margins(x=0.02, y=0.35)
+        self.ax.set_xlim(0, 100)  # will be updated based on MIDI length
+        self.ax.set_ylim(0, 127)  # MIDI note range
+        self.ax.axis("off")
 
-            self.ax.margins(x=0.02, y=0.35)
-            
-            self.ax.set_xlim(0, 100) # will be updated based on MIDI length
-            self.ax.set_ylim(0, 127) # MIDI note range
-            self.ax.axis('off')
+        self.canvas = FigureCanvas(self.fig)
+        self.layout = QVBoxLayout(self.midiframe)  # type: ignore
+        self.layout.setContentsMargins(0, 0, 0, 0)  # type: ignore
+        self.layout.addWidget(self.canvas)  # type: ignore
 
-            self.canvas = FigureCanvas(self.fig)
-            self.layout = QVBoxLayout(self.midiframe) # type: ignore
-            self.layout.setContentsMargins(0, 0, 0, 0) # type: ignore
-            self.layout.addWidget(self.canvas) # type: ignore
+        self.fig.patch.set_facecolor("#12131e")
+        self.ax.set_facecolor("#12131e")
+        self.ax.axis("off")
+        self.canvas.draw()
 
-            self.fig.patch.set_facecolor('#12131e')
-            self.ax.set_facecolor('#12131e')
-            self.ax.axis('off')
-            self.canvas.draw()
+        self.notes = [
+            (0.0, 1.0, 60),  # C4
+            (0.5, 0.5, 64),  # E4
+            (1.0, 1.0, 67),  # G4
+        ]
 
-            self.notes = [
-                    (0.0, 1.0, 60),  # C4
-                    (0.5, 0.5, 64),  # E4
-                    (1.0, 1.0, 67),  # G4
-            ]
+        self.midi_length = 0
 
-            self.midi_length = 0
+    def load_midi(self, midi_file): 
+        pass # TODO: will add later ;) for now just used set_notes with hardcoded notes for testing
 
-        def load_midi(self, midi_file): 
-              pass # TODO: will add later ;) for now just used set_notes with hardcoded notes for testing
+    def set_notes(self, notes):
+        self.notes = notes
 
-        def set_notes(self, notes):
-            self.notes = notes
+        self.ax.clear()
+        self.ax.set_facecolor("#12131e")
+        self.ax.axis("off")
 
-            self.ax.clear()
-
-            self.ax.set_facecolor('#12131e')
-            self.ax.axis('off')
-
-            for start, duration, note in self.notes:
-                rect = Rectangle(
+        for start, duration, note in self.notes:
+            rect = Rectangle(
                 (start, note - 0.45),
                 max(duration, 0.03),
                 0.9,
                 facecolor="#00b0a4",
                 edgecolor="#00ffb3",
-                linewidth=0.5
+                linewidth=0.5,
+            )
+            self.ax.add_patch(rect)
+
+        if self.notes:
+            max_time = 0
+            for note_data in self.notes:
+                start = note_data[0]
+                duration = note_data[1]
+                end_time = start + duration
+                if end_time > max_time:
+                    max_time = end_time
+
+            self.ax.set_xlim(0, max_time)
+
+            pitches = [note[2] for note in self.notes]
+            pitch_min = min(pitches)
+            pitch_max = max(pitches)
+            padding = 1
+
+            self.ax.set_ylim(
+                pitch_min - padding,
+                pitch_max + padding,
             )
 
-                self.ax.add_patch(rect)
+        self.canvas.draw()
 
-            if self.notes:
-                max_time = 0
-                for note_data in self.notes:
-                    start = note_data[0]
-                    duration = note_data[1]
-                    end_time = start + duration
-                    if end_time > max_time:
-                        max_time = end_time
-                
-                self.ax.set_xlim(0, max_time)
-
-                pitches = []
-
-                for note in self.notes:
-                    pitches.append(note[2])
-
-                    pitch_min = min(pitches)
-                    pitch_max = max(pitches)
-
-                    padding = 1
-
-                    self.ax.set_ylim(
-                        pitch_min - padding,
-                        pitch_max + padding
-                    )
-
-                self.canvas.draw()
 
